ee4d7511f271172a16a5e9b4af89cdf365c56d01
Anerikc <raptor142@gmail.com>
2022-09-09 15:38:53 +0300

    v3 added YT info
    YT info added to console for route /vidinfo

--- users/users.js
+++ users/users.js
@@ -1,9 +1,15 @@
 import express from 'express'
+import { metadata } from 'youtube-metadata-from-url';
+const url = 'https://youtu.be/8mJyNs5U-Xo';
 
 const usersRouter = express.Router()
 
 usersRouter.use((req, res, next) => {
-    console.log('Time: ', Date.now())
+    console.log(metadata(url).then(function(json) {
+        console.log(json);
+    }, function(err){
+        console.log(err);
+    }))
     next()
 })
 

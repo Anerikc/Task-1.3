e860f5023ab090c6fa44db87ba26e375fbf15b97
Anerikc <raptor142@gmail.com>
2022-09-09 15:36:24 +0300

    V2 Check data now
    Check Data now for /vidinfo

--- users/users.js
+++ users/users.js
@@ -2,6 +2,11 @@ import express from 'express'
 
 const usersRouter = express.Router()
 
+usersRouter.use((req, res, next) => {
+    console.log('Time: ', Date.now())
+    next()
+})
+
 usersRouter.get('/vidinfo', (req, res) => {
     res.send('Here is your info!')
 })
